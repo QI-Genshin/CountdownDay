3c8f8678c3bea13b7b04e5ac2f348e30e3dbfb94
Developer <dev@countdown.app>
2026-05-14 10:25:06 +0000

    Simplify build.gradle.kts and fix plugin resolution

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,13 @@
-buildscript {
-    repositories {
-        maven { url = uri("https://repo.huaweicloud.com/repository/maven") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/central") }
-        google()
-        mavenCentral()
-    }
-}
-
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
-allprojects {
-    repositories {
-        maven { url = uri("https://repo.huaweicloud.com/repository/maven") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/central") }
-        google()
-        mavenCentral()
-    }
+repositories {
+    google()
+    mavenCentral()
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
 }
