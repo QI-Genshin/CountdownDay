1e58ac8873b107a333074ec0e22da4bd663ca911
Developer <dev@countdown.app>
2026-05-14 10:32:45 +0000

    Remove repositories from root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
-repositories {
-    google()
-    mavenCentral()
-}
-
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
 }
